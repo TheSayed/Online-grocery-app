86d73aae0b0971a788afba4923f95fe6224d577a
TheSayed <kotp9a@gmail.com>
2024-07-25 16:41:27 +0300

    colors

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,4 +1,7 @@
 export const colors = {
   primary: "rgb(255, 94, 0)",
   secondary: "rgb(109, 56, 5)",
+  grey: "rgb(233, 233, 233)",
+  white: "rgb(255, 255, 255)",
+  shadow: "rgba(0, 0, 0, 0.06)",
 };
