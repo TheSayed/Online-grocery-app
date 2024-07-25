ccba81642cf4d98e199d05e3ff56ef33a1bd3998
TheSayed <kotp9a@gmail.com>
2024-07-25 16:40:20 +0300

    icons

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -1,3 +1,21 @@
-export const icons = {
+import { ImageRequireSource } from "react-native";
+
+interface Icons {
+  shop: ImageRequireSource;
+  explore: ImageRequireSource;
+  cart: ImageRequireSource;
+  favorite: ImageRequireSource;
+  account: ImageRequireSource;
+  location: ImageRequireSource;
+  backArrow: ImageRequireSource;
+}
+
+export const icons: Icons = {
   shop: require("./shop.png"),
+  explore: require("./explore.png"),
+  cart: require("./cart.png"),
+  favorite: require("./favorite.png"),
+  account: require("./account.png"),
+  location: require("./location.png"),
+  backArrow: require("./backArrow.png"),
 };
