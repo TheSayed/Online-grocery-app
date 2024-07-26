950c0ac4f489154e3f865f7d62e7214f7ff5d889
TheSayed <kotp9a@gmail.com>
2024-07-26 20:02:40 +0300

    fonts and packages

--- App.tsx
+++ App.tsx
@@ -3,8 +3,23 @@ import { StatusBar } from "expo-status-bar";
 import TabNavigator from "./src/navigators/TabNavigator";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import { useCallback } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import { fonts } from "./assets/fonts/fonts";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    ...fonts,
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
   return (
     <Provider store={store}>
       <NavigationContainer>
---
+++ assets/fonts/fonts.ts
@@ -0,0 +1,11 @@
+export const fonts = {
+  black900: require("./Inter-Black.ttf"),
+  bold700: require("./Inter-Bold.ttf"),
+  extraBold800: require("./Inter-ExtraBold.ttf"),
+  extraLight200: require("./Inter-ExtraLight.ttf"),
+  light300: require("./Inter-Light.ttf"),
+  medium500: require("./Inter-Medium.ttf"),
+  regular400: require("./Inter-Regular.ttf"),
+  semiBold600: require("./Inter-SemiBold.ttf"),
+  thin100: require("./Inter-Thin.ttf"),
+};
