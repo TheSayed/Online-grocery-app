0ef630a919b227f0c58818bf6bb28159f86853e1
TheSayed <kotp9a@gmail.com>
2024-07-27 15:09:28 +0300

    adding category type and words

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,8 +1,14 @@
 type Product = {
+  id?: number;
+  name?: string;
+  categoryId?: number;
+  price?: number;
+  unit?: string;
+  image?: string;
+};
+
+type Category = {
   id: number;
   name: string;
-  categoryId: number;
-  price: number;
-  unit: string;
   image: string;
 };
