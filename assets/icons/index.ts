c102eb84ca1dae74e487cc6e6a99ed9e7d951377
TheSayed <kotp9a@gmail.com>
2024-07-31 02:51:15 +0300

    Icons

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -4,20 +4,22 @@ interface Icons {
   shop: ImageRequireSource;
   explore: ImageRequireSource;
   cart: ImageRequireSource;
-  favorite: ImageRequireSource;
+  heart: ImageRequireSource;
   account: ImageRequireSource;
   location: ImageRequireSource;
   backArrow: ImageRequireSource;
   trash: ImageRequireSource;
+  search: ImageRequireSource;
 }
 
 export const icons: Icons = {
   shop: require("./shop.png"),
   explore: require("./explore.png"),
   cart: require("./cart.png"),
-  favorite: require("./favorite.png"),
+  heart: require("./heart.png"),
   account: require("./account.png"),
   location: require("./location.png"),
   backArrow: require("./backArrow.png"),
   trash: require("./trash-03.png"),
+  search: require("./search-03.png"),
 };
